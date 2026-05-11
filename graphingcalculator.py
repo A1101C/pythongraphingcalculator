914d013047dd04254129bc448bf3c4778a579224
AmmonC <ammoncrapo@outlook.com>
2026-05-10 23:57:53 -0600

    Asymptotes, Debugging
    New
    - Line tracking for displaying asymptotes correctly when lim x->0 for functions like 1/x, some cases may not work correctly at the current moment
    
    Known Issues
    Floating-point error accumulation

--- graphingcalculator.py
+++ graphingcalculator.py
@@ -1,4 +1,4 @@
-
+ls
 #_______________________________________________________troubleshooting Tools_______________________________________________________
 ts = 0 #troubelshooting, 1 is true 0 is false
 pv = 0 #print values, 1 makes it print the x and y values to the terminal
